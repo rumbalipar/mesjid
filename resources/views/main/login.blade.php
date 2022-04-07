d83b7820112a9537c5350bcec2107857d4857729
soleh.rumbalipar <soleh.rumbalipar@omni-hospitals.com>
2022-04-07 20:54:22 +0700

    Change Title Login

--- resources/views/main/login.blade.php
+++ resources/views/main/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Cafe</title>
+    <title>{{ config('app.name') }}</title>
     <link rel="shortcut icon" href="{{ url('/') }}/assets/images/favicon.ico">
     <link rel="stylesheet" href="{{ url('/') }}/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ url('/') }}/assets/fonts/font-awesome.min.css">
